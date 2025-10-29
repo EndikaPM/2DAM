746313eb8318963f94a5814127c1406c3beb28cc
EndikaPM <endika.perez.mas@gmail.com>
2025-10-29 07:18:32 +0100

    actualizacion movil entrega pojo

--- Movil/tema6/Tarea1&Tarea2/app/src/main/java/com/example/tarea1tarea2/MainActivity.java
+++ Movil/tema6/Tarea1&Tarea2/app/src/main/java/com/example/tarea1tarea2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.tarea1tarea2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +13,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String [] usuario = {"Endika", "Eva", "Usuario","Root"};
+        String [] password = {"Endika", "Eva", "Usuario", "root"};
+
+        Button ok = (Button) findViewById(R.id.ok);
+        Button singIn = (Button) findViewById(R.id.sign_In);
+        EditText textUser = (EditText) findViewById(R.id.textUsuario);
+        EditText textPassword = (EditText) findViewById(R.id.textPassword);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int tam = usuario.length;
+                String user = textUser.getText().toString();
+                String passwo = textPassword.getText().toString();
+                boolean registrado = false;
+                for (int i = 0; i < tam ; i++) {
+                    System.out.println(usuario[i]);
+                    System.out.println(user);
+                    if (usuario[i].toLowerCase().equals(user.toLowerCase())&& password[i].equals(passwo)){
+                        Intent pasarActivity2 = new Intent(MainActivity.this, Activity2.class);
+                        startActivity(pasarActivity2);
+                        registrado = true;
+                    }
+                }
+                if (!registrado) {
+                    Toast.makeText(MainActivity.this, "Usuario no registrado\n registrate para entrar", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
 }
