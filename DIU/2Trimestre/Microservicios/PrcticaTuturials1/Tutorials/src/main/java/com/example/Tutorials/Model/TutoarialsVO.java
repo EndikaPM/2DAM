005d9fdf757cadf5b68e991e20507f92c3983c90
EndikaPM <endika.perez.mas@gmail.com>
2026-02-05 07:37:47 +0100

    añadir Diu Microserivios

--- DIU/2Trimestre/Microservicios/PrcticaTuturials1/Tutorials/src/main/java/com/example/Tutorials/Model/TutoarialsVO.java
+++ DIU/2Trimestre/Microservicios/PrcticaTuturials1/Tutorials/src/main/java/com/example/Tutorials/Model/TutoarialsVO.java
@@ -1,15 +1,12 @@
 package com.example.Tutorials.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-
+@Data
 @Getter
 @Setter
 @AllArgsConstructor
