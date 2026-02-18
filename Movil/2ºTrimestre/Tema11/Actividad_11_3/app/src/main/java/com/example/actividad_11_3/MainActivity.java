165bcf1db3383d4daf592c0f74060f43048e0a43
EndikaPM <endika.perez.mas@gmail.com>
2026-02-18 20:51:57 +0100

    casitas de Eva

--- Movil/2ºTrimestre/Tema11/Actividad_11_3/app/src/main/java/com/example/actividad_11_3/MainActivity.java
+++ Movil/2ºTrimestre/Tema11/Actividad_11_3/app/src/main/java/com/example/actividad_11_3/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Dibujo11_3 divujo = new Dibujo11_3(this);
-        setContentView(divujo);
+        Dibujo11_3 dibujo = new Dibujo11_3(this);
+        setContentView(dibujo);
     }
 }
